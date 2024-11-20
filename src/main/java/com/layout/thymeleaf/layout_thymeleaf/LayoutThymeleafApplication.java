a1ba9cd66c2547f6fe85417b92bed1f008c179f9
Lucas Pereira <lpslucas27@gmail.com>
2024-11-20 08:55:56 -0300

    Project created

---
+++ src/main/java/com/layout/thymeleaf/layout_thymeleaf/LayoutThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.layout.thymeleaf.layout_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayoutThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayoutThymeleafApplication.class, args);
+	}
+
+}
